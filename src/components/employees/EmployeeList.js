72966dd140469ef98b751eb3e6c77376871f460a
ts <shanksleslietrey@gmail.com>
2021-08-27 15:34:25 -0500

    added delete/fire button for employee list

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -5,13 +5,23 @@ export const EmployeeList = () => {
     const [employees, setEmpoyees] = useState([])
     const history = useHistory()
 
+    const deleteEmployee = (id) => {
+        fetch(`http://localhost:8088/employees/${id}`, {
+            method: "DELETE"
+        })
+        .then(getEmployees)
+    }
+
+    const getEmployees = () => {
+        return     fetch("http://localhost:8088/employees?_expand=store")
+        .then(res => res.json())
+        .then((data) => {
+            setEmpoyees(data)
+        })
+    }
     useEffect(
         () => {
-            fetch("http://localhost:8088/employees?_expand=store")
-                .then(res => res.json())
-                .then((data) => {
-                    setEmpoyees(data)
-                })
+            getEmployees()
         },
         []
     )
@@ -30,7 +40,7 @@ export const EmployeeList = () => {
                 employees.map(
                     (employee) => {
                         return <p key={`employee--${employee.id}`}>
-                        {employee.name} {employee.store.address} 
+                        {employee.name} {employee.store.address} <button onClick={() => {deleteEmployee(employee.id)}}>Fire Employee</button>
                         </p>
                     }
                 )
